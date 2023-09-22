f5ea577aa85254456a8c310e0cacc5b022e7b905
Yan Lucas Cavalcante Rodrigues <yanlucr@gmail.com>
2023-09-21 22:22:38 -0400

    Add spring template

---
+++ src/main/java/com/esufam/megami/MegamiApplication.java
@@ -0,0 +1,13 @@
+package com.esufam.megami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esufam/megami/MegamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esufam.megami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
