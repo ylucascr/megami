07f167b52dce3ef0d5fb72146ca10ac071d64cd8
Yan Lucas Cavalcante Rodrigues <yanlucr@gmail.com>
2023-11-02 14:46:56 -0400

    Add get endpoint to validate token

--- src/main/java/com/esufam/megami/controllers/AuthenticationController.java
+++ src/main/java/com/esufam/megami/controllers/AuthenticationController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,11 @@ public class AuthenticationController {
 
     @Autowired
     private TokenService tokenService;
+
+    @GetMapping(path = "/login")
+    public ResponseEntity<Response> isLoggedIn() {
+        return ResponseEntity.ok(Response.success(null));
+    }
 
     @PostMapping(path = "/login")
     public ResponseEntity<Response> login(@RequestBody AuthDTO userData) {
