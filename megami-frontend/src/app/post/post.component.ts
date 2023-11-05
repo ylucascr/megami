91fee9a2cbf9771974fb6b3e2f9613232418c498
Yan Lucas Cavalcante Rodrigues <yanlucr@gmail.com>
2023-11-05 05:12:58 -0400

    Fix post not using api from environment

--- megami-frontend/src/app/post/post.component.ts
+++ megami-frontend/src/app/post/post.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { DataService } from '../data.service';
+import { environment } from 'environments/environments';
 
 @Component({
   selector: 'app-post',
@@ -10,6 +11,8 @@ import { DataService } from '../data.service';
 export class PostComponent {
   @Input({ required: true }) post: any;
   @Output() postDeleted: EventEmitter<any> = new EventEmitter();
+
+  apiUrl = environment.apiUrl || 'http://localhost:8080'
 
   constructor(
     public authService: AuthService,
